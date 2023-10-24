a8b6d0f4fffd32fa60be4e450feba2b98365e9e4
Heyjoo3 <heyjoo.3@gmail.com>
2023-10-24 14:05:41 +0200

    base header and styling

--- finance-plan/src/App.vue
+++ finance-plan/src/App.vue
@@ -1,29 +1,49 @@
 <template>
-  <p>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
-    necessitatibus rem perspiciatis facilis amet odit quae corporis, corrupti
-    doloremque qui placeat deserunt voluptatem non? Libero esse totam laboriosam
-    accusamus quos voluptate quae iusto, officiis quasi officia asperiores quis,
-    illum id quisquam consectetur dolores eum vel impedit mollitia
-    exercitationem sint harum voluptatum amet! Non nobis expedita ut quas in
-    vero illo at enim labore explicabo temporibus natus molestias odit unde
-    doloribus sed eius earum laborum quo minima nihil odio, quod id? Dolores
-    iure voluptatum alias magni deserunt expedita molestias, culpa eaque quia
-    placeat tenetur necessitatibus amet dignissimos doloribus optio autem earum
-    doloremque blanditiis illo dolor sunt praesentium. Eos voluptate expedita
-    fugiat nisi dicta, possimus molestiae fugit accusamus impedit ratione ad
-    iusto iste pariatur ex iure. Vel praesentium culpa excepturi ipsam expedita
-    voluptate officia, perferendis voluptatem minus neque dolores ipsum ea
-    molestiae. Tenetur, corporis, soluta corrupti ullam quod numquam maiores
-    neque quam architecto accusamus nulla voluptatum ducimus temporibus? Facere,
-    tempore sed. Exercitationem illum, eos voluptates dolore nostrum, amet, unde
-    tempora doloremque laudantium quod veritatis nihil dolores nemo at ex quia
-    natus blanditiis? Velit nostrum possimus repellendus voluptate, dignissimos
-    error, aliquid maiores totam in suscipit atque eos sapiente corporis quam,
-    ducimus omnis officiis.
-  </p>
+  <the-header></the-header>
+  <div class="content">
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
+      necessitatibus rem perspiciatis facilis amet odit quae corporis, corrupti
+      doloremque qui placeat deserunt voluptatem non? Libero esse totam
+      laboriosam accusamus quos voluptate quae iusto, officiis quasi officia
+      asperiores quis, illum id quisquam consectetur dolores eum vel impedit
+      mollitia exercitationem sint harum voluptatum amet! Non nobis expedita ut
+      quas in vero illo at enim labore explicabo temporibus natus molestias odit
+      unde doloribus sed eius earum laborum quo minima nihil odio, quod id?
+      Dolores iure voluptatum alias magni deserunt expedita molestias, culpa
+      eaque quia placeat tenetur necessitatibus amet dignissimos doloribus optio
+      autem earum doloremque blanditiis illo dolor sunt praesentium. Eos
+      voluptate expedita fugiat nisi dicta, possimus molestiae fugit accusamus
+      impedit ratione ad iusto iste pariatur ex iure. Vel praesentium culpa
+      excepturi ipsam expedita voluptate officia, perferendis voluptatem minus
+      neque dolores ipsum ea molestiae. Tenetur, corporis, soluta corrupti ullam
+      quod numquam maiores neque quam architecto accusamus nulla voluptatum
+      ducimus temporibus? Facere, tempore sed. Exercitationem illum, eos
+      voluptates dolore nostrum, amet, unde tempora doloremque laudantium quod
+      veritatis nihil dolores nemo at ex quia natus blanditiis? Velit nostrum
+      possimus repellendus voluptate, dignissimos error, aliquid maiores totam
+      in suscipit atque eos sapiente corporis quam, ducimus omnis officiis.
+    </p>
+  </div>
 </template>
 
-<script></script>
+<script>
+import TheHeader from "./components/header/TheHeader.vue";
+export default { components: { TheHeader } };
+</script>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  background-color: #152032;
+  /* background-color: #17253e; */
+}
+p {
+  color: #ecf0f3;
+}
+</style>
