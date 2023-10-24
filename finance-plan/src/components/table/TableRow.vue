08aa826ac70499916d7ea73e16401caaf83940d3
nico <nico.schulz1337@gmail.com>
2023-10-24 13:10:01 +0200

    table

---
+++ finance-plan/src/components/table/TableRow.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tableRow">
+    <p>{{ data.date }}</p>
+    <p>{{ data.amount }}€</p>
+    <p>{{ data.description }}</p>
+    <p>{{ data.category }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["data"],
+};
+</script>
+
+<style scoped>
+.tableRow {
+  display: flex;
+  flex-direction: row;
+  border: 1px solid black;
+  padding: 1vh;
+}
+
+p {
+  width: 25%;
+  text-align: center;
+}
+</style>
