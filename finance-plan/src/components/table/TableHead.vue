f9cb83d5a5a2ed73bfba5ca3e4812adf8e1379f4
nico <nico.schulz1337@gmail.com>
2023-10-24 13:09:22 +0200

    table

---
+++ finance-plan/src/components/table/TableHead.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tableHead">
+    <h3>Datum</h3>
+    <h3>Betrag</h3>
+    <h3>Bezeichnung</h3>
+    <h3>Kategorie</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["data"],
+};
+</script>
+
+<style scoped>
+.tableHead {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  background-color: black;
+  color: white;
+  padding: 2vh;
+}
+
+h3 {
+  width: 25%;
+  text-align: center;
+}
+</style>
