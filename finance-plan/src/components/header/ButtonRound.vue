60aa9da54725b46ac9a90bbe333baaad4259a707
nico <nico.schulz1337@gmail.com>
2023-10-25 12:37:10 +0200

    fixes

--- finance-plan/src/components/header/ButtonRound.vue
+++ finance-plan/src/components/header/ButtonRound.vue
@@ -12,7 +12,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 button {
   background-color: transparent;
   height: 2rem;
--- finance-plan/src/components/nav/NavLeft.vue
+++ finance-plan/src/components/nav/NavLeft.vue
@@ -36,7 +36,6 @@ export default {
     return {
       open: false,
       closeNavTimeout: null,
-      closeNavTimeout: null,
     };
   },
 
