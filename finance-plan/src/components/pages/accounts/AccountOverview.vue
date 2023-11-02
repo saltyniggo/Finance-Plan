80ea5c70e278787c1c83e19200c780f175a0a9ae
Heyjoo3 <heyjoo.3@gmail.com>
2023-11-02 16:31:00 +0100

    unbeendete account overview

---
+++ finance-plan/src/components/pages/accounts/AccountOverview.vue
@@ -0,0 +1,81 @@
+<template>
+  <base-card>
+    <h2>Accounts</h2>
+    <div v-for="account in accounts" :key="account.id" class="row">
+      <button class="navBtn">
+        <span>{{ account.name }}</span
+        ><span>{{ account.balance }}</span>
+      </button>
+      <button class="editBtn"><i class="fa-solid fa-pen"></i></button>
+      <button class="editBtn"><i class="fa-solid fa-trash"></i></button>
+    </div>
+    <button class="addBtn" style="backgroundcolor: #17253e">
+      + Account hinzufügen
+    </button>
+  </base-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      accounts: [
+        {
+          name: "Sparkasse",
+          balance: 70,
+          id: 1,
+        },
+        {
+          name: "Volksbank",
+          balance: -70,
+          id: 2,
+        },
+        {
+          name: "Commerz",
+          balance: 700,
+          id: 3,
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+h2 {
+  color: #ecf0f3;
+}
+.row {
+  width: 90%;
+  display: inline-flex;
+}
+
+.navBtn {
+  width: 90%;
+  padding: 2%;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1rem;
+  border-radius: 20px;
+  background: linear-gradient(135deg, #20639b, #05da93);
+  color: #ecf0f3;
+  font-weight: 500;
+  font-size: 2vh;
+  text-shadow: 2px 2px 3px #1512032;
+}
+
+.editBtn {
+  width: 5%;
+  padding: 2%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+  border-radius: 20px;
+  background: #05da93;
+  color: #ecf0f3;
+  font-weight: 500;
+  font-size: 2vh;
+  text-shadow: 2px 2px 3px #1512032;
+  margin-left: 1%;
+}
+</style>
--- finance-plan/src/store/index.js
+++ finance-plan/src/store/index.js
@@ -3,12 +3,14 @@ import { createStore } from "vuex";
 import transactionModule from "./modules/transactions/index.js";
 import registerModule from "./modules/register/index.js";
 import userModule from "./modules/user";
+import accountsModule from "./modules/accounts/index.js";
 
 const store = createStore({
   modules: {
     transactionList: transactionModule,
     registerModule: registerModule,
     userModule: userModule,
+    accountsModule: accountsModule,
   },
   state() {
     return {
