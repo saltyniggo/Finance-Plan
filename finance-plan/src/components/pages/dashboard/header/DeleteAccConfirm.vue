054da243ae890bf33b6c2eaeee3c2c02ebecd366
Heyjoo3 <heyjoo.3@gmail.com>
2023-11-08 10:45:49 +0100

    confirm delte account begonnen

---
+++ finance-plan/src/components/pages/dashboard/header/DeleteAccConfirm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="confirmationDialog">
+    <p>Sicher?</p>
+    <button id="confirmDelete">Ja</button>
+    <button id="cancelDelete">Nein</button>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+p {
+  margin-right: 20%;
+}
+
+button {
+  margin-right: 5%;
+}
+</style>
