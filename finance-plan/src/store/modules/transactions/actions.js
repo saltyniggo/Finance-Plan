122ba86a0de640ade62378431e9814d24186f36f
Heyjoo3 <heyjoo.3@gmail.com>
2023-11-08 09:52:54 +0100

    copy axiso from accounts to transactions

--- finance-plan/src/store/modules/transactions/actions.js
+++ finance-plan/src/store/modules/transactions/actions.js
@@ -1,4 +1,65 @@
+import TransactionService from "@/store/service/transactionService.js";
 export default {
+  async deleteTransaction(context, accId) {
+    await TransactionService.deleteTransaction(accId)
+      .then((response) => {
+        if (response == "successful") {
+          console.log("delte");
+          context.commit("deleteTransaction", accId);
+        } else if (response == "unsuccessful") {
+          console.log("delete not possible");
+        }
+      })
+      .catch((error) => {
+        console.log("conenction problem", error);
+      });
+  },
+
+  async addTransaction({ commit }, payload) {
+    await TransactionService.addTransaction(payload)
+      .then((response) => {
+        if (response == "successful") {
+          console.log("addTransaction");
+          commit("TransactionModule/addTransaction", payload);
+        } else if (response == "unsucessful") {
+          console.error("ERROR");
+        }
+      })
+      .catch((error) => {
+        console.error("connection problem", error);
+      });
+  },
+
+  async editTransaction({ commit }, { accId, edit }) {
+    await TransactionService.putTransactionEdit(accId, edit)
+      .then((response) => {
+        if (response == "successful") {
+          console.log("editTransaction");
+          commit("editTransaction", { accId, edit });
+        } else if (response == "unsucessful") {
+          console.error("ERROR");
+        }
+      })
+      .catch((error) => {
+        console.error("connection problem", error);
+      });
+  },
+
+  async getTransactions({ commit }) {
+    await TransactionService.getTransactions()
+      .then((response) => {
+        if (response == "successful") {
+          console.log("editTransaction");
+          commit("setTransactions");
+        } else if (response == "unsucessful") {
+          console.error("ERROR");
+        }
+      })
+      .catch((error) => {
+        console.error("connection problem", error);
+      });
+  },
+
   deleteTransaction(context, value) {
     context.commit("deleteTransaction", value);
   },
--- finance-plan/src/store/service/transactionService.js
+++ finance-plan/src/store/service/transactionService.js
@@ -1,17 +1,79 @@
-async function postFeed(feedname) {
-  const response = await axios({
-    method: "post",
-    url: "https://io.adafruit.com/api/v2/angelos12345/feeds/",
-    headers: { "X-AIO-Key": "aio_kcGI202lAbtvIsWVhfyx21H8Kp6P" },
-    data: {
-      feed: {
-        name: feedname,
+import axios from "axios";
+
+async function addTransaction(transactionNameInput) {
+  try {
+    const response = await axios({
+      method: "post",
+      url: "https://io.adafruit.com/api/v2/angelos12345/feeds/",
+      headers: { "X-AIO-Key": "aio_kcGI202lAbtvIsWVhfyx21H8Kp6P" },
+      data: {
+        data: {
+          transactionName: transactionNameInput,
+        },
       },
-    },
-  });
-  return response;
+    });
+    return response;
+  } catch (error) {
+    console.error("Error when adding Transaction to user in DB:", error);
+    throw error;
+  }
+}
+async function deleteTransaction(transactionId) {
+  try {
+    const response = await axios({
+      method: "delete",
+      url: "https://io.adafruit.com/api/v2/angelos12345/feeds/",
+      // url: `https://io.adafruit.com/api/v2/angelos12345/feeds/${TransactionId}`,
+      headers: { "X-AIO-Key": "aio_kcGI202lAbtvIsWVhfyx21H8Kp6P" },
+      data: {
+        transactionId: transactionId,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log("Error when deleting Transaction in DB");
+    console.error("Error:", error);
+    throw error;
+  }
+}
+
+async function putTransactionEdit(transactionId, transactionNameInput) {
+  try {
+    const response = await axios({
+      method: "delete",
+      url: "https://io.adafruit.com/api/v2/angelos12345/feeds/",
+      headers: { "X-AIO-Key": "aio_kcGI202lAbtvIsWVhfyx21H8Kp6P" },
+      data: {
+        transactionId: transactionId,
+        transactionNameInput,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log("Error when editing Transaction in DB");
+    console.error("Error:", error);
+    throw error;
+  }
+}
+
+async function getTransactions() {
+  try {
+    const response = await axios({
+      method: "get",
+      url: "https://io.adafruit.com/api/v2/angelos12345/feeds/",
+      headers: { "X-AIO-Key": "aio_kcGI202lAbtvIsWVhfyx21H8Kp6P" },
+    });
+    return response;
+  } catch (error) {
+    console.log("Error when editing Transaction in DB");
+    console.error("Error:", error);
+    throw error;
+  }
 }
 
 export default {
-  postFeed,
+  addTransaction,
+  deleteTransaction,
+  putTransactionEdit,
+  getTransactions,
 };
